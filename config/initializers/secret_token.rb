366419e04e8b504feec0d747f25de01df132c074
Josemar Luedke <josemarluedke@gmail.com>
2014-02-14 16:21:29 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TamoJunto::Application.config.secret_key_base = 'eb2515bd7aabdf0ab638bc4817a7eba05c2a7ebe93089a80dbf81523f49a7a915de05a2a3a2f85535d37115b8d30b06a40e8a1a18522f56ec658e811e49275c1'

2ce2322ab1db50730cb916d2b7004dfc1b83dbff
Josemar Luedke <josemarluedke@gmail.com>
2014-02-17 10:09:04 -0300

    Add facebook key and secret to settings table

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,6 +251,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, 'APP_ID', 'APP_SECRET', scope: 'email', image_size: 'normal'
+  config.omniauth :facebook, Setting['omniauth.facebook.id'], Setting['omniauth.facebook.secret'], scope: 'email', image_size: 'normal'
 
 end

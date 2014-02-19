b8f55e02a240587e6ff18e6a84beabfe6fbc8a3c
Josemar Luedke <josemarluedke@gmail.com>
2014-02-19 10:17:06 -0300

    Add admin base controller

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,10 @@
+class Admin::BaseController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :verify_admin
+
+  private
+  def verify_admin
+    redirect_to root_path, flash: { error: 'You are not authorized to access this page!' } unless current_user.admin?
+  end
+
+end
--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -2,11 +2,24 @@ require 'spec_helper'
 
 describe Admin::DashboardController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  context 'when user is not admin' do
+    let(:user) { User.make! }
+    before do
+      sign_in user
+      get :index
     end
+
+    it { expect(response).to redirect_to(root_path) }
+    it { expect(flash[:error]).to eq 'You are not authorized to access this page!' }
   end
 
+  context 'when user is not admin' do
+    let(:user) { User.make! admin: true }
+    before do
+      sign_in user
+      get :index
+    end
+
+    it { expect(response).to be_success }
+  end
 end

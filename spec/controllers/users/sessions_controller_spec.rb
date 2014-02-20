2da33ecc16c997aafa744076db0c655dd8b86ad3
Rafael Barboza <barboza.cardoso@gmail.com>
2014-02-20 18:28:51 -0300

    Add specs to check when reactivating an user

---
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Devise::SessionsController do
+  describe 'Reactivate User' do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @user = User.make!
+      @user.update_attribute(:active, false)
+      post :create, user: { email: @user.email, password: 'niceguy123' }
+      @user.reload
+    end
+
+    it { expect(@user.active?).to be_true }
+  end
+end

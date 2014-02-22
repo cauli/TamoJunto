d2dfa0776a38543bcdfd4a8ffad90914a815e0d9
Josemar Luedke <josemarluedke@gmail.com>
2014-02-21 22:21:34 -0300

    Add beautiful_text to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
+  include Shared::BeautifulText
   validates :title, :content, presence: true
+  beautiful_text_for [:content]
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,4 +5,9 @@ describe Article do
     it{ should validate_presence_of :title }
     it{ should validate_presence_of :content }
   end
+
+  describe 'auto_html and beautiful text' do
+    subject { Article.make! }
+    it { expect(subject.content_html).not_to be_nil }
+  end
 end

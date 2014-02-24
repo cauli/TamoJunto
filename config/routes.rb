30740e8d18a9793e6bbb2f882175ae28e5570882
Rafael Barboza <barboza.cardoso@gmail.com>
2014-02-24 17:27:38 -0300

    Add the show page for the Video

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ TamoJunto::Application.routes.draw do
   resources :users, only: :show
   resources :organizations, only: :show
   resources :articles, only: :show
+  resources :videos, only: :show
 
   namespace :admin do
     get '/', to: 'dashboard#index'

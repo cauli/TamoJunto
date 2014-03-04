a33e6e450c791d33a9caef837ad174895d3954dc
Josemar Luedke <josemarluedke@gmail.com>
2014-03-04 00:36:57 -0300

    Add admin menu

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,13 @@
 class Admin::BaseController < ApplicationController
   before_filter :authenticate_user!
   before_filter :verify_admin
+  helper_method :admin?
 
   private
+  def admin?
+    true
+  end
+
   def verify_admin
     redirect_to root_path, flash: { error: 'You are not authorized to access this page!' } unless current_user.admin?
   end

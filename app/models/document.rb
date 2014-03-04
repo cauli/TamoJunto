cdb680732ec28c965016bc9d3aff304df184332e
Rafael Barboza <barboza.cardoso@gmail.com>
2014-03-03 21:34:46 -0300

    Add the file type verification to the file

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ActiveRecord::Base
   validates :title, :file, presence: true
+  validates_property :ext, of: :file, in: %w{doc docx odt pages rtf txt tex csv key pps ppt pptx pdf xlr xls xlsx xml}, message: I18n.t('document.invalid_extension')
 
   dragonfly_accessor :file
 end

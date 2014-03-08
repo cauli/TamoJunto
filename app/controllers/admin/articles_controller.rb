f1b881f1852d493c647503c3921dab3ceeb2ae16
Josemar Luedke <josemarluedke@gmail.com>
2014-03-08 14:46:48 -0300

    Use content_html on articles form

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -5,6 +5,6 @@ class Admin::ArticlesController < Admin::BaseController
 
   protected
   def permitted_params
-    params.permit(article: [:title, :image, :content, :content_html_new])
+    params.permit(article: [:title, :image, :content_html])
   end
 end

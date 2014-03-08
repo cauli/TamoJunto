da8f334658f8d46f1df85ab9aa12a25415a77644
Josemar Luedke <josemarluedke@gmail.com>
2014-03-08 14:45:35 -0300

    Remove beautiful_text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,4 @@
 class Article < ActiveRecord::Base
-  include Shared::BeautifulText
   validates :title, :content, presence: true
-  beautiful_text_for [:content]
   dragonfly_accessor :image
-
-  def content_html_test
-    self.content_html
-  end
-  def content_html_test=(html)
-    self.content = ReverseMarkdown.parse(html)
-  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,11 +6,6 @@ describe Article do
     it{ should validate_presence_of :content }
   end
 
-  describe 'auto_html and beautiful text' do
-    subject { Article.make! }
-    it { expect(subject.content_html).not_to be_nil }
-  end
-
   describe '#image with dragonfly' do
     let(:article) { Article.make! image: File.open("#{Rails.root}/spec/fixtures/image.png") }
     it { expect(article.image.url).to be_present }

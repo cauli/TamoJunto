60f333a64abc0c9c27f8dcbdacb9570e1dde144b
Josemar Luedke <josemarluedke@gmail.com>
2014-03-09 17:55:24 -0300

    Create migration for events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227211339) do
+ActiveRecord::Schema.define(version: 20140309204701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,21 @@ ActiveRecord::Schema.define(version: 20140227211339) do
     t.string   "description"
     t.string   "file_uid"
     t.string   "file_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image_uid"
+    t.text     "local"
+    t.date     "starts_at"
+    t.date     "ends_at"
+    t.string   "time"
+    t.string   "external_link"
+    t.float    "value"
+    t.string   "state"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -78,6 +93,24 @@ ActiveRecord::Schema.define(version: 20140227211339) do
   end
 
   add_index "settings", ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true, using: :btree
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"

702fb7f19a3910852f22e54b48ac25db5f96d759
Josemar Luedke <josemarluedke@gmail.com>
2014-03-09 19:04:56 -0300

    Add image uploader

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   include Event::StateMachineHandler
   validates :name, :description, :local, :starts_at, :ends_at, :time, presence: true
+  dragonfly_accessor :image
 end

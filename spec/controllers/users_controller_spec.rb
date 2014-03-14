13c8d1368dd32dcc105a4c8236777e2dc4efcaa1
Rafael Barboza <barboza.cardoso@gmail.com>
2014-03-14 15:16:37 -0300

    Add controller specs for the upvote and downvote pages

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,4 +6,42 @@ describe UsersController do
     before { get :show, id: user.id }
     it { expect(response).to be_success}
   end
+
+  describe '#upvotes' do
+    let(:article) { Article.make! }
+    let(:document) { Document.make! }
+    let(:video) { Video.make! }
+
+    before do
+      user.vote_for(article)
+      user.vote_for(document)
+      user.vote_for(video)
+      sign_in user
+      get :upvotes
+    end
+
+    it { expect(response).to be_success }
+    it { expect(assigns(:articles)).to eq [article] }
+    it { expect(assigns(:documents)).to eq [document] }
+    it { expect(assigns(:videos)).to eq [video] }
+  end
+
+  describe '#downvotes' do
+    let(:article) { Article.make! }
+    let(:document) { Document.make! }
+    let(:video) { Video.make! }
+
+    before do
+      user.vote_against(article)
+      user.vote_against(document)
+      user.vote_against(video)
+      sign_in user
+      get :downvotes
+    end
+
+    it { expect(response).to be_success }
+    it { expect(assigns(:articles)).to eq [article] }
+    it { expect(assigns(:documents)).to eq [document] }
+    it { expect(assigns(:videos)).to eq [video] }
+  end
 end

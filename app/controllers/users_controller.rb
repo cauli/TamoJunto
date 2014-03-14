334e0f3d339abeb4a3f2f4de2549afc70dda8a84
Rafael Barboza <barboza.cardoso@gmail.com>
2014-03-14 12:36:32 -0300

    Add the retrieving methods to the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   inherit_resources
   actions :show
+  before_filter :authenticate_user!, only: [:upvotes, :downvotes]
+
+  def upvotes
+    @articles = current_user.upvoted_articles
+    @documents = current_user.upvoted_documents
+    @videos = current_user.upvoted_videos
+  end
+
+  def downvotes
+    @articles = current_user.downvoted_articles
+    @documents = current_user.downvoted_documents
+    @videos = current_user.downvoted_videos
+  end
 end

df2b1112aa1d9f042709b21b851173b56cbf3fd7
Rafael Barboza <barboza.cardoso@gmail.com>
2014-03-14 00:12:00 -0300

    Removes unused code

--- app/controllers/concerns/shared/voteable_actions.rb
+++ app/controllers/concerns/shared/voteable_actions.rb
@@ -7,7 +7,6 @@ module Shared::VoteableActions
 
     def upvote
       return unless resource.present?
-      return unless user_signed_in?
 
       current_user.vote_exclusively_for(resource)
 
@@ -20,7 +19,6 @@ module Shared::VoteableActions
 
     def downvote
       return unless resource.present?
-      return unless user_signed_in?
 
       current_user.vote_exclusively_against(resource)
 

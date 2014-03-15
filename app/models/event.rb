c694fb43ef40315c64dc406c0f6d1c7680d2bbef
Josemar Luedke <josemarluedke@gmail.com>
2014-03-15 14:03:54 -0300

    Add tags to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ActiveRecord::Base
   belongs_to :organization
   validates :name, :description, :local, :starts_at, :ends_at, :time, :organization_id, presence: true
   dragonfly_accessor :image
+  acts_as_taggable
 end

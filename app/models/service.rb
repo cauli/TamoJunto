bbc68728ed0f44057ada564d3758fd5808087630
Josemar Luedke <josemarluedke@gmail.com>
2014-03-15 13:23:52 -0300

    Add image uploader to service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ActiveRecord::Base
   include Service::StateMachineHandler
-
   belongs_to :organization
   validates :name, :description, :local, :organization_id, presence: true
+  dragonfly_accessor :image
 end

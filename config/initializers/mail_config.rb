360b905f75b06f13ad9a60d28d74b42699cd1c67
Josemar Luedke <josemarluedke@gmail.com>
2014-04-16 22:09:25 -0300

    Create config for mailers using maildril

---
+++ config/initializers/mail_config.rb
@@ -0,0 +1,17 @@
+begin
+  ActionMailer::Base.default 'Content-Transfer-Encoding' => 'quoted-printable'
+
+  if Rails.env.production?
+    ActionMailer::Base.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_APIKEY'],
+    domain: 'heroku.com'
+    }
+    ActionMailer::Base.delivery_method = :smtp
+  end
+rescue
+  nil
+end

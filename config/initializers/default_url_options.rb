72eaaf895705e01ae27679b3fa27530483a66c3b
Josemar Luedke <josemarluedke@gmail.com>
2014-04-23 09:31:32 -0300

    Change project url on mail and devise initializers

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-  ActionMailer::Base.asset_host = 'tamojunto.com.br'
-  Rails.application.routes.default_url_options = { host: 'tamojunto.com.br' }
+  ActionMailer::Base.asset_host = 'tamojunto.org.br'
+  Rails.application.routes.default_url_options = { host: 'tamojunto.org.br' }
 else
   Rails.application.routes.default_url_options = { host: 'localhost:3000' }
 end

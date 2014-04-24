f79b60169b7218eea0ef97331f0168c08cf87dbd
Rafael Barboza <barboza.cardoso@gmail.com>
2014-04-24 11:12:56 -0300

    Fix some association errors

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :topic
   belongs_to :theme
   validates :theme_id, :topic_id, :question_text,
-            :good_answer, :bad_answer, required: true
+            :good_answer, :bad_answer, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Question do
   describe 'Validations' do
     it { should validate_presence_of :theme_id }
-    it { should validate_presence_of :question_id }
+    it { should validate_presence_of :topic_id }
     it { should validate_presence_of :question_text }
     it { should validate_presence_of :good_answer }
     it { should validate_presence_of :bad_answer }

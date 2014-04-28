03df5215391376200fc82c043c46354e7ede5f92
Rafael Barboza <barboza.cardoso@gmail.com>
2014-04-28 14:39:11 -0300

    Fix the hash syntax

--- db/migrate/20140425194039_create_related_themes.rb
+++ db/migrate/20140425194039_create_related_themes.rb
@@ -5,7 +5,7 @@ class CreateRelatedThemes < ActiveRecord::Migration
       t.integer :related_theme_id
     end
 
-    add_index(:related_themes, [:theme_id, :related_theme_id], :unique => true)
-    add_index(:related_themes, [:related_theme_id, :theme_id], :unique => true)
+    add_index(:related_themes, [:theme_id, :related_theme_id], unique: true)
+    add_index(:related_themes, [:related_theme_id, :theme_id], unique: true)
   end
 end

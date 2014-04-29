e012aab2d47439ad1baa0666553325187546ae0a
Josemar Luedke <josemarluedke@gmail.com>
2014-04-29 20:14:01 -0300

    Add #new, #create, #edit and #update to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,52 @@
 class EventsController < ApplicationController
+  after_filter :verify_authorized, except: [:show]
   inherit_resources
-  actions :show
+  actions :all, except: :index
   respond_to :html
+
+  def new
+    @event = Event.new(organization: current_organization)
+    authorize @event
+    respond_with @event
+  end
+
+  def create
+    @event = Event.new(permitted_params[:event].merge(organization: current_organization))
+    authorize @event
+    create!
+  end
+
+  def edit
+    authorize resource
+    edit!
+  end
+
+  def update
+    authorize resource
+    update!
+  end
+
+  def destroy
+    authorize resource
+    destroy!
+  end
+
+  protected
+  def permitted_params
+    params.permit(event: [:name,
+                          :image,
+                          :description,
+                          :local,
+                          :starts_at,
+                          :ends_at,
+                          :time,
+                          :organization_id,
+                          :external_link,
+                          :value,
+                          :tag_list])
+  end
+
+  def pundit_user
+    current_organization
+  end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -10,4 +10,42 @@ describe EventsController do
     it { expect(assigns(:event)).to eq event }
   end
 
+  context 'authenticated as organization' do
+    let(:organization) { Organization.make! }
+    let(:event) { Event.make!(organization: organization) }
+    before { sign_in organization }
+
+    describe '#new' do
+      before { get :new }
+      it { expect(response).to be_success }
+      it { expect(assigns(:event)).to be_new_record }
+    end
+
+    describe '#create' do
+      before { post :create, event: event_params }
+      it { expect(response).to redirect_to(event_path(Event.last)) }
+    end
+
+    describe '#edit' do
+      before { get :edit, id: event }
+      it { expect(response).to be_success }
+      it { expect(assigns(:event)).to eq event }
+    end
+
+    describe '#update' do
+      before { post :update, id: event, event: event_params }
+      it { expect(response).to redirect_to(event_path(event)) }
+    end
+
+    describe '#destroy' do
+      before { delete :destroy, id: event }
+      it { expect(response).to redirect_to(root_path) }
+      it { expect { event.reload }.to raise_error }
+    end
+  end
+
+  protected
+  def event_params
+    Event.make.attributes.merge(organization_id: [organization.id])
+  end
 end

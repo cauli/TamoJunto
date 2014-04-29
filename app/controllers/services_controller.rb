f4a2292719301d9d796191330563f741a3669462
Josemar Luedke <josemarluedke@gmail.com>
2014-04-29 20:05:00 -0300

    Add #new, #create, #edit and #update to services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,46 @@
 class ServicesController < ApplicationController
+  after_filter :verify_authorized, except: [:show]
   inherit_resources
-  actions :show
+  actions :all, except: :index
   respond_to :html
+
+  def new
+    @service = Service.new(organization: current_organization)
+    authorize @service
+    respond_with @service
+  end
+
+  def create
+    @service = Service.new(permitted_params[:service].merge(organization: current_organization))
+    authorize @service
+    create!
+  end
+
+  def edit
+    authorize resource
+    edit!
+  end
+
+  def update
+    authorize resource
+    update!
+  end
+
+  def destroy
+    authorize resource
+    destroy!
+  end
+
+  protected
+  def permitted_params
+    params.permit(service: [:name, :image, :description,
+                            :local, :external_link,
+                            :value,
+                            :tag_list])
+  end
+
+  def pundit_user
+    current_organization
+  end
 end
 

c8d1eecc9b0421a3aa32f4f0116f82372a174b76
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-06 17:31:05 -0300

    Make the related theme required

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :topic
   belongs_to :theme
   belongs_to :related_theme, class_name: :Theme, foreign_key: :related_theme_id
-  validates :theme_id, :topic_id, :question_text,
+  validates :theme_id, :related_theme_id,:topic_id, :question_text,
             :good_answer, :bad_answer, presence: true
 end

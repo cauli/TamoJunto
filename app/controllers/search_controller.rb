83775afa57b17e1a11fba08136521f2b84c57d2b
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-13 17:55:59 -0300

    Change the params reading to stop throwing nil errors

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @articles = Article.search(params[:search][:query])
-    @documents = Document.search(params[:search][:query])
-    @events = Event.search(params[:search][:query])
-    @services = Service.search(params[:search][:query])
-    @videos = Video.search(params[:search][:query])
+    query = params[:search].present? ? params[:search][:query] : nil
+    @articles = Article.search(query)
+    @documents = Document.search(query)
+    @events = Event.search(query)
+    @services = Service.search(query)
+    @videos = Video.search(query)
   end
 end
 

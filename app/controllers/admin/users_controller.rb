19f673067d7299a29452a6465d79a6b580bc5fee
Josemar Luedke <josemarluedke@gmail.com>
2014-05-13 10:30:18 -0300

    Add black list of attributes on organization adn user admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,6 +9,25 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def permitted_params
-    params.permit(user: Organization.attribute_names.map(&:to_sym))
+    params.permit(user: Organization.attribute_names.map(&:to_sym) - attributes_black_list)
+  end
+
+  def attributes_black_list
+    [:id,
+     :confirmed_at,
+     :confirmation_token,
+     :confirmation_sent_at,
+     :created_at,
+     :updated_at,
+     :encrypted_password,
+     :reset_password_token,
+     :reset_password_sent_at,
+     :remember_created_at,
+     :sign_in_count,
+     :current_sign_in_at,
+     :last_sign_in_at,
+     :current_sign_in_ip,
+     :last_sign_in_ip
+     ]
   end
 end

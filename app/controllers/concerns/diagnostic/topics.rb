1f19877fb1d30e2daa8d27eb54aae638b6140c57
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-15 01:45:53 -0300

    Extract more of the topics logic into funcions

--- app/controllers/concerns/diagnostic/topics.rb
+++ app/controllers/concerns/diagnostic/topics.rb
@@ -4,13 +4,7 @@ module Diagnostic::Topics
 
   included do
     def main_topics
-      themes.order(:id).each_with_index.map do |theme, index|
-        if bad_topics[index].present?
-          bad_topics[index].first
-        else
-          theme.questions.order(:id).first.topic
-        end
-      end
+      themes.order(:id).each_with_index.map { |t, i| select_main_topic(t, i) }
     end
 
     private
@@ -20,6 +14,14 @@ module Diagnostic::Topics
         answers.by_theme(theme).order(:question_id).bad.map do |a|
           a.try(:question).try(:topic)
         end
+      end
+    end
+
+    def select_main_topic(theme, index)
+      if bad_topics[index].present?
+          bad_topics[index].first
+      else
+        theme.questions.order(:id).first.topic
       end
     end
   end

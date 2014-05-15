8592eb4e0113f76e7624c0f975ed53a0eb68b1be
Josemar Luedke <josemarluedke@gmail.com>
2014-05-15 12:09:02 -0300

    Show organization's events and services

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,16 @@
 class OrganizationsController < ApplicationController
   inherit_resources
   actions :show
+
+  def show
+    if current_organization == resource
+      @events = resource.events
+      @services = resource.services
+    else
+      @events = resource.events.visible
+      @services = resource.services.with_state('visible')
+    end
+
+    show!
+  end
 end

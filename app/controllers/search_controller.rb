6a74e758aee8a8a9249ae4d6810907a97f8b7315
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-28 17:04:00 -0300

    Make the search only return visible events and services

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
     query = params[:search].present? ? params[:search][:query] : nil
     @articles = Article.search(query)
     @documents = Document.search(query)
-    @events = Event.search(query)
+    @events = Event.visible.search(query)
     @topics = Topic.search(query)
-    @services = Service.search(query)
+    @services = Service.visible.search(query)
     @videos = Video.search(query)
   end
 end

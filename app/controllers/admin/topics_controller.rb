83519221e423528d9d08cff0e553b12f6dd7c27f
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-28 10:54:57 -0300

    Make topics sortable by drag and drop

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -3,12 +3,19 @@ class Admin::TopicsController < Admin::BaseController
   actions :all, except: :show
   respond_to :html
 
+  def sort
+    resource.update_attribute :row_order_position, params[:topic][:row_order_position]
+
+    render nothing: true
+  end
+
   protected
 
   def permitted_params
     params.permit(topic: [:title,
                           :description,
                           :tag_list,
+                          :row_order_position,
                           article_ids: [],
                           document_ids: [],
                           event_ids: [],

63d8e18c51391e276d85bdd238fe266b3792b37e
Rafael Barboza <barboza.cardoso@gmail.com>
2014-06-13 22:09:07 -0300

    Add the new home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    redirect_to user_path(current_user) if user_signed_in?
+    redirect_to organization_path(current_organization) if organization_signed_in?
   end
 end

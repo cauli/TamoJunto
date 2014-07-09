22eb58481b925051c27feac06aeb4a8d9d8e3911
Rafael Barboza <barboza.cardoso@gmail.com>
2014-07-09 00:02:29 -0300

    Fix admin translations and question relations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :topic
   belongs_to :theme
   has_and_belongs_to_many :related_themes, class_name: :Theme
-  has_and_belongs_to_many :diagnostics
   has_many :questions
 
   validates :theme_id, :topic_id, :question_text,

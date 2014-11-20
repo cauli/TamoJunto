e9756edc6e811d54e030367a19339cbe5132b377
Rafael Barboza <barboza.cardoso@gmail.com>
2014-11-20 13:49:11 -0200

    Fix again the recover password url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ TamoJunto::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.action_mailer.default_url_options = { :host => 'www.tamojunto.org.br' }
 end
